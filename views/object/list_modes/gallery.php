05caf299aa0769dadcb43d3b276317d4b4339c85
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2016-08-02 15:12:58 -0300

    improved gallery list mode view

--- views/object/list_modes/gallery.php
+++ views/object/list_modes/gallery.php
@@ -1,41 +1,53 @@
-<div class="col-md-3 gallery-view-container top-div <?php if ( ($countLine % 4) == 1 ) { ?> first-el <?php  } ?>"
-     <?php if ($collection_list_mode != "gallery"): ?> style="display: none" <?php endif ?> >
-     <div class="row">
-         <div class="item-thumb">
-             <a href="<?php echo get_collection_item_href($collection_id); ?>"
-                onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
-                 <?php echo get_item_thumb_image($curr_id); ?>
-             </a>
-         </div>
-     </div>
+<?php
+    $additional_curr_class = "";
+    if ( ($countLine % 4) == 1 ) { 
+        $additional_curr_class = "first-el";
+    } else if( ($countLine % 4) == 0 ) {
+        $additional_curr_class = "last-el";
+    }    
+?>
 
-     <div class="row title-container">
-         <h5 class="item-display-title">
-             <a href="<?php echo get_collection_item_href($collection_id); ?>"
-                onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
-                 <?php echo wp_trim_words( get_the_title(), 4 ); ?>
-             </a>
-         </h5>
-         
-         <div class="gallery-metadata">
-             <div class="col-md-5 no-padding">
-                 <?php if (get_option('collection_root_id') != $collection_id): ?>                
-                     <!-- TAINACAN: container(AJAX) que mostra o html com os rankings do objeto-->
-                     <div id="r_gallery_<?php echo $curr_id ?>" class="rankings-container"></div>                     
-                 <?php endif; ?>
-             </div>
-             <ul class="item-funcs col-md-6 right">
-                 <input type="hidden" class="post_id" name="post_id" value="<?= $curr_id ?>">
-                 <li>
-                     <a id="modal_network<?php echo $curr_id; ?>" onclick="showModalShareNetwork(<?php echo $curr_id; ?>)">
-                         <div style="cursor:pointer;" data-icon="&#xe00b;"></div>
-                     </a>
-                 </li>
+<div class="col-md-3 gallery-view-container top-div <?php echo $additional_curr_class ?>"     
+    <?php if ($collection_list_mode != "gallery"): ?> style="display: none" <?php endif ?> >
+    
+    <div class="gallery-wrapper">
+        <div class="">
+            <div class="item-thumb">
+                <a href="<?php echo get_collection_item_href($collection_id); ?>"
+                   onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
+                    <?php echo get_item_thumb_image($curr_id); ?>
+                </a>
+            </div>
+        </div>
 
-                 <?php include "edit_btns.php"; ?>
-             </ul>
+        <div class=" title-container">
+            <h5 class="item-display-title">
+                <a href="<?php echo get_collection_item_href($collection_id); ?>"
+                   onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
+                    <?php echo wp_trim_words( get_the_title(), 4 ); ?>
+                </a>
+            </h5>
 
-         </div>
-         
-     </div>
+            <div class="gallery-metadata">
+                <div class="col-md-5 no-padding">
+                    <?php if (get_option('collection_root_id') != $collection_id): ?>                
+                        <!-- TAINACAN: container(AJAX) que mostra o html com os rankings do objeto-->
+                        <div id="r_gallery_<?php echo $curr_id ?>" class="rankings-container"></div>                     
+                    <?php endif; ?>
+                </div>
+                <ul class="item-funcs col-md-6 right">
+                    <input type="hidden" class="post_id" name="post_id" value="<?= $curr_id ?>">
+                    <li>
+                        <a id="modal_network<?php echo $curr_id; ?>" onclick="showModalShareNetwork(<?php echo $curr_id; ?>)">
+                            <div style="cursor:pointer;" data-icon="&#xe00b;"></div>
+                        </a>
+                    </li>
+
+                    <?php include "edit_btns.php"; ?>
+                </ul>
+
+            </div>         
+        </div>
+        
+    </div>
  </div>
