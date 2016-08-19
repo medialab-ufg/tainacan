77150aaa3db9a8a445c44e73ae6cebaeced8c315
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2016-08-19 13:37:48 -0300

    fix

--- views/object/js/geolocation_js.php
+++ views/object/js/geolocation_js.php
@@ -2,84 +2,77 @@
   var use_approx_mode = $("#approx_mode").val();
   var objs = $(".object_id");
   var ids = [],
-    locations = [],
     lats = [],
     longs = [];   
-    
-    cl($(objs));
-    
-  // use metadata with approximated location   
-  if(use_approx_mode == "use_approx_mode") {
-      $(objs).each(function(idx, el) {      
-        var id = $(el).val();
-        ids[idx] = id;    
 
-        var location = $("#object_" + id + " .location").val();
-        var title = "<div class='col-md-12'>"+$.trim( $("#object_" + id ).html() )+"</div>";
+  var marker_item = [];
+  var tot = 0;
 
-        if(location) {
-            cl("Me vê aqui as coordenadas de " + location + " ... ");    
-            var search_url = "http://maps.google.com/maps/api/geocode/json?address=" + location + "&sensor=false";
+  $(objs).each(function(idx, el) {
+      var current_id = $(el).val();
+      var current_content   = "<div class='col-md-12'>"+ $.trim( $("#object_" + current_id ).html() )+ "</div>";      
+
+    if ( use_approx_mode && use_approx_mode == "use_approx_mode" ) {
+        var current_location  = $("#object_" + current_id + " .location").val();
+        cl(current_location);
+
+        if(current_location) {
+            var search_url = "http://maps.google.com/maps/api/geocode/json?address=" + current_location + "&sensor=false";
             $.getJSON(search_url, function(data) {
                 $.each( data.results, function( key, val ) {
                     var lt = val.geometry.location.lat;
                     var lng = val.geometry.location.lng;
-                //items.push( "<li id='" + key + "'> Lat: " + lt + " ; Long:" + lng +" </li>" );
-                cl('Ok ... tua latitude eh ' + lt  + ' e tua longitude eh ' + lng);
+                                        
+                    if (lt && lng) {
+                        marker_item[idx] = [ current_content, lt, lng ];
+                        lats[idx] = parseFloat(lt);
+                        longs[idx] = parseFloat(lng);                        
+                    }
+                    cl("I got " + lats[idx]);
+                    cl("I got " + longs[idx]);
+                    cl(marker_item[idx]);
+                    tot++;
+                    cl("O total disso é : " + tot + " de modo aproximado.");
               });
-            });        
-        } else {
-            // cl(id);    
+            });
         }
-
-          if(lat && long) {
-            locations[idx] = [title, lat, long];
-            lats[idx] = parseFloat(lat);
-            longs[idx] = parseFloat(long);
-          }
-      });
+        
+    } else {
+        var current_latitude  = $("#object_" + current_id + " .latitude").val();
+        var current_longitude = $("#object_" + current_id + " .longitude").val();
       
-  } else {
-      $(objs).each(function(idx, el) {      
-        var id = $(el).val();
-        ids[idx] = id;    
-
-        var lat =  $("#object_" + id + " .latitude").val();
-        var long = $("#object_" + id + " .longitude").val();
-        var title = "<div class='col-md-12'>"+$.trim( $("#object_" + id ).html() )+"</div>";
-
-          if(lat && long) {
-            locations[idx] = [title, lat, long];
-            lats[idx] = parseFloat(lat);
-            longs[idx] = parseFloat(long);
-          }
-      });
-    // use metadata configured for latitude and longitude
+        if(current_latitude && current_longitude) {
+            marker_item[idx] = [ current_content, current_latitude, current_longitude ];
+            lats[idx] = parseFloat(current_latitude);
+            longs[idx] = parseFloat(current_longitude);
+        }        
+    }
+      
+  });
+    
     var sorted_lats = lats.sort(function(a,b) { return a - b; } );
     var sorted_longs = longs.sort(function(a,b) { return a - b; } );
-    var half_length = parseInt( locations.length / 2 );
+    var half_length = parseInt( marker_item.length / 2 );
 
-      function initMap() {
-        //document.getElementById('map').style.display = "block";
+    function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: half_length,
-          center: new google.maps.LatLng( sorted_lats[half_length] ,sorted_longs[half_length]),
+          zoom: half_length, center: new google.maps.LatLng( sorted_lats[half_length] ,sorted_longs[half_length]),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
 
         var infowindow = new google.maps.InfoWindow();
         var marker, i;
 
-        for (i = 0; i < locations.length; i++) {
-          if(locations[i]) {
+        for (i = 0; i < marker_item.length; i++) {
+          if(marker_item[i]) {
             marker = new google.maps.Marker({
-              position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+              position: new google.maps.LatLng(marker_item[i][1], marker_item[i][2]),
               map: map
             });
 
             google.maps.event.addListener(marker, 'click', (function (marker, i) {
               return function () {
-                infowindow.setContent(locations[i][0]);
+                infowindow.setContent(marker_item[i][0]);
                 infowindow.open(map, marker);
               };
             })(marker, i));   
@@ -87,10 +80,7 @@
         } // for
     }
 
-    initMap();
-  } 
-
-  
+    initMap();  
   
   /*
   if(lats && lats.length < 1 ) {
