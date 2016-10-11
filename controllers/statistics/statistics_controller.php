a6fa8433186a216824e08ec24a464e6ec6a1584e
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2016-10-11 12:28:33 -0300

    base structure statistics

---
+++ controllers/statistics/statistics_controller.php
@@ -0,0 +1,23 @@
+<?php
+
+class StatisticsController extends Controller {
+    public function operation($op, $data) {
+        switch($op):
+            case "show_statistics":
+                return $this->render(dirname(__FILE__) . '../../../views/statistics/list.php', $data);
+        endswitch;
+        
+    }
+    
+}
+
+if ($_POST['operation']) {
+    $operation = $_POST['operation'];
+    $data = $_POST;
+} else {
+    $operation = $_GET['operation'];
+    $data = $_GET;
+}
+
+$stat_controller = new StatisticsController();
+echo $stat_controller->operation($operation, $data);
