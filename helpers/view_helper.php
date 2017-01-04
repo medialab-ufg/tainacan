6a3b9c22bcb952a8ccbb0bd9a57868a56a067ec5
Rodrigo de Oliveira <rodrigodeoliveira@hotmail.com.br>
2017-01-04 20:36:40 -0200

    Better slideshow design, more flexible for different kind of object types and sizes

--- helpers/view_helper.php
+++ helpers/view_helper.php
@@ -382,7 +382,7 @@ class ViewHelper {
         $_sec = explode("&", $_fst[1]);
         $_video_id = $_sec[0];
 
-        return "http://www.youtube.com/embed/" . $_video_id . "?html5=1";
+        return "http://www.youtube.com/embed/" . $_video_id . "?html5=1?autoplay=1";
     }
 
     public function videoSlideItemHtml($_item_id) {
@@ -407,7 +407,7 @@ class ViewHelper {
                     return;
                 }
             }
-            echo "<div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='$videoURL' frameborder='0'></iframe></div>";
+            echo "<div class='embed-responsive embed-responsive-16by9'><iframe class='embed-responsive-item' src='$videoURL'></iframe></div>";
         } else {
             echo get_item_thumb_image($_item_id, "large");
         }
