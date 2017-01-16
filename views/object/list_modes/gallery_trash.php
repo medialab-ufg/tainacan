84eb059e3ae58d2f9d6af00d211635e91457dfcd
mbrunodm <mbrunodm@hotmail.com>
2017-01-16 16:34:37 -0200

    Trash List

---
+++ views/object/list_modes/gallery_trash.php
@@ -0,0 +1,52 @@
+<?php
+    $additional_curr_class = "";
+    if ( ($countLine % 4) == 1 ) { 
+        $additional_curr_class = "first-el";
+    } else if( ($countLine % 4) == 0 ) {
+        $additional_curr_class = "last-el";
+    }    
+?>
+
+<div class="col-md-3 gallery-view-container top-div <?php echo $additional_curr_class ?>"   
+     data-order="<?php echo $countLine; ?>"  
+     <?php if ($collection_list_mode != "gallery"): ?> style="display: none" <?php endif ?> >
+    
+    <input type="hidden" id="add_classification_allowed_<?php echo get_the_ID() ?>" name="add_classification_allowed" value="<?php echo (string) verify_allowed_action($collection_id, 'socialdb_collection_permission_add_classification', get_the_ID()); ?>" />
+    <div class="gallery-wrapper droppableClassifications toggleSelect">
+        <div class="item-thumb">
+            <a href="<?php echo get_collection_item_href($collection_id); ?>"
+               onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
+                <?php echo get_item_thumb_image($curr_id); ?>
+            </a>
+        </div>
+
+        <div class=" title-container">
+            <h5 class="item-display-title">
+                <a href="<?php echo get_collection_item_href($collection_id); ?>"
+                   onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
+                    <?php echo wp_trim_words( get_the_title(), 4 ); ?>
+                </a>
+            </h5>
+
+            <div class="gallery-metadata">
+                <div class="col-md-5 no-padding">
+                    <?php if (get_option('collection_root_id') != $collection_id): ?>                
+                        <!-- TAINACAN: container(AJAX) que mostra o html com os rankings do objeto-->
+                        <?php if(has_action('container_rankings_gallery')): ?>
+                            <?php do_action('container_rankings_gallery',$curr_id) ?>
+                        <?php else: ?>
+                            <div id="r_gallery_<?php echo $curr_id ?>" class="rankings-container"></div>
+                        <?php endif; ?>                    
+                    <?php endif; ?>
+                </div>
+                <ul class="item-funcs col-md-6 right">
+                    <input type="hidden" class="post_id" name="post_id" value="<?= $curr_id ?>">
+
+                    <?php include "edit_btns_trash.php"; ?>
+                </ul>
+
+            </div>         
+        </div>
+        
+    </div>
+ </div>
---
+++ views/object/list_modes/list_trash.php
@@ -0,0 +1,49 @@
+<div class="droppableClassifications col-md-12 no-padding list-view-container top-div toggleSelect" 
+    data-order="<?php echo $countLine; ?>"
+    <?php if ($collection_list_mode != "list"): ?> style="display: none" <?php endif ?> >
+    
+    <input type="hidden" id="add_classification_allowed_<?php echo $curr_id ?>" 
+            name="add_classification_allowed"
+            value="<?php echo (string) verify_allowed_action($collection_id, 'socialdb_collection_permission_add_classification', $curr_id); ?>" />
+    
+    <div>
+        <div class="col-md-1 item-thumb">
+            <a href="<?php echo get_collection_item_href($collection_id); ?>"
+               onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
+                <?php echo get_item_thumb_image($curr_id); ?>
+            </a>
+        </div>
+        <div class="col-md-4 no-padding">
+            <h4 class="item-display-title">
+                <a href="<?php echo get_collection_item_href($collection_id); ?>" style="color: black; font-weight: bolder"
+                   onclick="<?php get_item_click_event($collection_id, $curr_id )?>">
+                    <?php the_title(); ?>
+                </a>
+            </h4>
+        </div>
+
+        <div class="col-md-3 author-created">
+            <div class="item-author"><?php echo "<strong>" . __('Created by: ', 'tainacan') . "</strong>" . get_the_author(); ?></div>
+            <div class="item-creation"><?php echo $curr_date ?></div>
+        </div>
+
+        <div class="col-md-2 no-padding">
+            <?php if (get_option('collection_root_id') != $collection_id): ?>
+                <!-- TAINACAN: container(AJAX) que mostra o html com os rankings do objeto-->
+                <?php if(has_action('container_rankings_list')): ?>
+                    <?php do_action('container_rankings_list',$curr_id) ?>
+                <?php else: ?>
+                        <div id="r_list_<?php echo $curr_id ?>" class="rankings-container"></div> 
+                <?php endif; ?>  
+            <?php endif; ?>
+        </div>
+
+        <div class="col-md-2">
+            <ul class="item-funcs right">
+                <input type="hidden" class="post_id" name="post_id" value="<?= $curr_id ?>">
+                
+                <?php include "edit_btns_trash.php"; ?>          
+            </ul>
+        </div>
+    </div>
+</div>
