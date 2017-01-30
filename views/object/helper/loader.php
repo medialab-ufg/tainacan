a29cd276847ba10872fa41104cfb71b4fa517a44
Rodrigo de Oliveira <rodrigodeoliveira@hotmail.com.br>
2017-01-30 16:50:59 -0200

    Improve collection's home organization

---
+++ views/object/helper/loader.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Loads variables used along different collection's visualization's modes
+ */
+$countLine = 0;
+$collection_list_mode = $collection_data['collection_metas']['socialdb_collection_list_mode'];
+
+if( !$collection_list_mode ) {
+    $collection_list_mode = "cards";
+}
+
+if("geolocation" === $collection_list_mode && is_null($geo_coordinates) && ($is_filtered_page || $pagid)) {
+    $geo_coordinates["lat"] = get_post_meta($col_id, "socialdb_collection_latitude_meta", true);
+    $geo_coordinates["long"] = get_post_meta($col_id, "socialdb_collection_longitude_meta", true);
+
+    echo '<input type="hidden" id="filtered_collection" value="true" />';
+}
+$_fxd_title = $viewHelper->terms_fixed['title']->name;
+$numberItems = ceil($loop->found_posts / 10);
+?>
+
+<!-- TAINACAN: hidden utilizados para execucao de processos desta view (list.php)  -->
+<input type="hidden" id="keyword_pagination" name="keyword_pagination" value="<?php if (isset($keyword)) echo $keyword; ?>" />
+<input type="hidden" id="sorted_form" name="sorted_form" value="<?php echo $sorted_by; ?>" />
+<input type="hidden" id="default-viewMode" value="<?php echo $collection_list_mode; ?>">
+<input type="hidden" id="temp-viewMode" value="<?php echo $collection_list_mode; ?>">
+<input type="hidden" id="slideshow-time" value="<?php echo $_slideshow_time; ?>">
+<input type="hidden" id="set-lat" value="<?php echo $geo_coordinates["lat"]; ?>">
+<input type="hidden" id="set-long" value="<?php echo $geo_coordinates["long"]; ?>">
+<input type="hidden" id="approx_mode" value="<?php echo $use_approx_mode; ?>">
+<input type="hidden" id="approx_location" value="<?php echo $geo_loc; ?>">
+<input type="hidden" id="repo_fixed_title" value="<?php echo $_fxd_title; ?>">
+<input type="hidden" id="original_post_count" value="<?php echo $loop->post_count; ?>" />
+<input type="hidden" id="pagination_current_page" value="<?php echo $pagid; ?>" />
--- views/object/list.php
+++ views/object/list.php
@@ -3,42 +3,11 @@ include_once('./../../helpers/view_helper.php');
 include_once('./../../helpers/object/object_helper.php');
 include_once ('js/list_js.php');
 include_once ('js/geolocation_js.php');
+include_once ('helper/loader.php');
 
 $viewHelper = new ViewHelper();
 $objHelper = new ObjectHelper();
 
-$countLine = 0;
-$collection_list_mode = $collection_data['collection_metas']['socialdb_collection_list_mode'];
-
-if( !$collection_list_mode ) {
-    $collection_list_mode = "cards";
-}
-
-if("geolocation" === $collection_list_mode && is_null($geo_coordinates) && ($is_filtered_page || $pagid) ) {
-    $geo_coordinates["lat"] = get_post_meta($col_id, "socialdb_collection_latitude_meta", true);
-    $geo_coordinates["long"] = get_post_meta($col_id, "socialdb_collection_longitude_meta", true);
-
-    echo '<input type="hidden" id="filtered_collection" value="true" />';
-}
-$_fxd_title = $viewHelper->terms_fixed['title']->name;
-$numberItems = ceil($loop->found_posts / 10);
-?>
-
-<!-- TAINACAN: hidden utilizados para execucao de processos desta view (list.php)  -->
-<input type="hidden" id="keyword_pagination" name="keyword_pagination" value="<?php if (isset($keyword)) echo $keyword; ?>" />
-<input type="hidden" id="sorted_form" name="sorted_form" value="<?php echo $sorted_by; ?>" />
-<input type="hidden" id="default-viewMode" value="<?php echo $collection_list_mode; ?>">
-<input type="hidden" id="temp-viewMode" value="<?php echo $collection_list_mode; ?>">
-<input type="hidden" id="slideshow-time" value="<?php echo $_slideshow_time; ?>">
-<input type="hidden" id="set-lat" value="<?php echo $geo_coordinates["lat"]; ?>">
-<input type="hidden" id="set-long" value="<?php echo $geo_coordinates["long"]; ?>">
-<input type="hidden" id="approx_mode" value="<?php echo $use_approx_mode; ?>">
-<input type="hidden" id="approx_location" value="<?php echo $geo_loc; ?>">
-<input type="hidden" id="repo_fixed_title" value="<?php echo $_fxd_title; ?>">
-<input type="hidden" id="original_post_count" value="<?php echo $loop->post_count; ?>" />
-<input type="hidden" id="pagination_current_page" value="<?php echo $pagid; ?>" />
-
-<?php
 $objHelper->renderCollectionPagination($loop->found_posts, $loop->post_count, $pagid, $show_string, 'top_pag');
 
 if ( $loop->have_posts() ): ?>
