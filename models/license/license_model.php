b9eba76f4673f0a8f2d10d2d0683578d133357d0
mbrunodm <mbrunodm@hotmail.com>
2017-02-06 16:06:41 -0200

    Model Format

--- models/license/license_model.php
+++ models/license/license_model.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once (dirname(__FILE__) . '/../../../../../wp-config.php');
 include_once (dirname(__FILE__) . '/../../../../../wp-load.php');
 include_once (dirname(__FILE__) . '/../../../../../wp-includes/wp-db.php');
@@ -26,45 +27,42 @@ class LicenseModel {
         wp_update_post($post);
         update_post_meta($data['editLicenseId'], 'socialdb_custom_license_url', $data['add_license_url']);
 
-        $result['title'] = __('Success','tainacan');
-        $result['msg'] = __('Edit successfully','tainacan');
+        $result['title'] = __('Success', 'tainacan');
+        $result['msg'] = __('Edit successfully', 'tainacan');
         $result['type'] = 'success';
 
         return $result;
     }
 
     public function delete_repository_license($id, $collection_id) {
-
         //DELETAR
         wp_delete_post($id);
         delete_post_meta($collection_id, 'socialdb_collection_license', $id);
 
-        $result['title'] = __('Success','tainacan');
-        $result['msg'] = __('Delete successfully','tainacan');
+        $result['title'] = __('Success', 'tainacan');
+        $result['msg'] = __('Delete successfully', 'tainacan');
         $result['type'] = 'success';
 
         return $result;
     }
 
     public function change_pattern_license($id, $collection_id) {
-
         update_post_meta($collection_id, 'socialdb_collection_license_pattern', $id);
 
-        $result['title'] = __('Success','tainacan');
-        $result['msg'] = __('Change pattern successfully','tainacan');
+        $result['title'] = __('Success', 'tainacan');
+        $result['msg'] = __('Change pattern successfully', 'tainacan');
         $result['type'] = 'success';
 
         return $result;
     }
 
     public function change_enabled_license($data) {
-
         parse_str($data['form_data'], $arr);
         $enabledLicenses = serialize($arr['enabledLicense']);
         update_post_meta($data['collection_id'], 'socialdb_collection_license_enabled', $enabledLicenses);
 
-        $result['title'] = __('Success','tainacan');
-        $result['msg'] = __('Change enabled license successfully','tainacan');
+        $result['title'] = __('Success', 'tainacan');
+        $result['msg'] = __('Change enabled license successfully', 'tainacan');
         $result['type'] = 'success';
 
         return $result;
@@ -73,7 +71,7 @@ class LicenseModel {
     public function get_repository_licenses($collection_id) {
         $arrLicenses = get_option('socialdb_standart_licenses');
         $pattern = get_post_meta($collection_id, 'socialdb_collection_license_pattern');
-        if(isset($arrLicenses)&&!empty($arrLicenses)){
+        if (isset($arrLicenses) && !empty($arrLicenses)) {
             foreach ($arrLicenses as $license) {
                 $object_post = get_post($license);
                 $data_license['id'] = $object_post->ID;
@@ -83,7 +81,7 @@ class LicenseModel {
             }
         }
         $arrLicenses_custom = get_option('socialdb_custom_licenses');
-        if(isset($arrLicenses_custom)&&!empty($arrLicenses_custom)){
+        if (isset($arrLicenses_custom) && !empty($arrLicenses_custom)) {
             foreach ($arrLicenses_custom as $license) {
                 $object_post = get_post($license);
                 $data_license['id'] = $object_post->ID;
@@ -171,8 +169,8 @@ class LicenseModel {
 
         add_post_meta($data['collection_id'], 'socialdb_collection_license', $object_id);
 
-        $result['title'] = __('Success','tainacan');
-        $result['msg'] = __('Registered successfully','tainacan');
+        $result['title'] = __('Success', 'tainacan');
+        $result['msg'] = __('Registered successfully', 'tainacan');
         $result['type'] = 'success';
 
         return $result;
