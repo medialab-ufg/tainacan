40eec70f8848f85b6c57a9831e9025722f71655a
Rodrigo Guimarães <rodrigodeoliveira@hotmail.com.br>
2017-05-23 14:27:02 -0300

    0001419: Ordem Listagem Eventos

--- views/event/list.php
+++ views/event/list.php
@@ -6,10 +6,8 @@ if($collection_id == get_option('collection_root_id')){
 }else{
     $url = get_the_permalink($collection_id);
 }
-
 ?>
 <div id="events_title" class="col-md-12">
-
     <div class="col-md-12 config_default_style" id="events_settings">
         <?php ViewHelper::render_config_title( __("Events", 'tainacan'), $url ); ?>
 
@@ -22,7 +20,7 @@ if($collection_id == get_option('collection_root_id')){
             <?php _e('Error! Operation was unsuccessful.', 'tainacan') ?>&nbsp;<span id="message_category"></span>
         </div>
 
-        <div class="events_menu" class="">
+        <div class="events_menu">
             <div class="col-md-12">
                 <div role="tabpanel">
                     <!-- Nav tabs -->
@@ -153,10 +151,7 @@ if($collection_id == get_option('collection_root_id')){
                                         <th><?php _e('Event Type', 'tainacan'); ?></th>
                                         <th><?php _e('Event Description', 'tainacan'); ?></th>
                                         <th><?php _e('State', 'tainacan'); ?></th>
-                                        <?php /* ?>
-                                        <th><?php _e('Approval Date', 'tainacan'); ?></th>
-                                        <th><?php _e('Approved by', 'tainacan'); ?></th>
-                                        <?php */ ?>
+                                        <th><?php _e('Real Date', 'tainacan'); ?></th>
                                     </tr>
                                     </thead>
                                     <tbody id="table_events_verified" >
@@ -193,6 +188,7 @@ if($collection_id == get_option('collection_root_id')){
                                                     ?>
                                                 </td>
                                             <?php } ?>
+                                            <td> <?php echo date($event['date']); ?> </td>
                                             <?php /* ?>
                                             <td>
                                                 <?php if ($event['state'] != 'invalid'): ?>
