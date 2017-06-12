0c0da381cdba6d6a9c38b76ffaf59072cc83458a
Eduardo <eduardo.humberto1992@gmail.com>
2017-06-12 18:31:39 -0300

    begin category metadata extra

--- views/object/formItem/input/checkbox.class.php
+++ views/object/formItem/input/checkbox.class.php
@@ -5,10 +5,55 @@ class CheckboxClass extends FormItem{
         ?>
         <?php if($property['has_children'] && is_array($property['has_children'])): ?>
             <?php foreach ($property['has_children'] as $child): ?>
-                <input type="checkbox" 
-                       name="checkbox-field-<?php echo $compound_id ?>-<?php echo $property['id'] ?>-<?php echo $index_id; ?>[]" 
+                <input type="checkbox"
+                       name="checkbox-field-<?php echo $compound_id ?>-<?php echo $property['id'] ?>-<?php echo $index_id; ?>[]"
                        value="<?php echo $child->term_id ?>">&nbsp;<?php echo $child->name ?>
             <?php endforeach; ?>
-        <?php endif; 
+        <?php endif;
+        $this->initScriptsCheckboxBoxClass($compound_id, $property_id, $item_id, $index_id);
+    }
+
+    /**
+     *
+     * @param type $property
+     * @param type $item_id
+     * @param type $index
+     */
+    public function initScriptsCheckboxBoxClass($compound_id,$property_id, $item_id, $index_id) {
+        ?>
+        <script>
+            $('input[name="checkbox-field-<?php echo $compound_id ?>-<?php echo $property_id ?>-<?php echo $index_id; ?>"]').change(function(){
+                 if($(this).is(':checked')){
+                   $.ajax({
+                       url: $('#src').val() + '/controllers/object/form_item_controller.php',
+                       type: 'POST',
+                       data: {
+                           operation: 'saveValue',
+                           type:'term',
+                           value: $(this).val(),
+                           item_id:'<?php echo $item_id ?>',
+                           compound_id:'<?php echo $compound_id ?>',
+                           property_children_id: '<?php echo $property_id ?>',
+                           index: <?php echo $index_id ?>
+                       }
+                   });
+                 }else{
+                    $.ajax({
+                       url: $('#src').val() + '/controllers/object/form_item_controller.php',
+                       type: 'POST',
+                       data: {
+                           operation: 'removeValue',
+                           type:'term',
+                           value: $(this).val(),
+                           item_id:'<?php echo $item_id ?>',
+                           compound_id:'<?php echo $compound_id ?>',
+                           property_children_id: '<?php echo $property_id ?>',
+                           index: <?php echo $contador ?>
+                       }
+                   });
+                 }
+            });
+        </script>
+        <?php
     }
 }
--- views/object/formItem/input/radio.class.php
+++ views/object/formItem/input/radio.class.php
@@ -5,10 +5,40 @@ class RadioClass extends FormItem{
         ?>
         <?php if($property['has_children'] && is_array($property['has_children'])): ?>
             <?php foreach ($property['has_children'] as $child): ?>
-                <input type="radio" 
-                       name="radio-field-<?php echo $compound_id ?>-<?php echo $property['id'] ?>-<?php echo $index_id; ?>[]" 
+                <input type="radio"
+                       name="radio-field-<?php echo $compound_id ?>-<?php echo $property['id'] ?>-<?php echo $index_id; ?>[]"
                        value="<?php echo $child->term_id ?>">&nbsp;<?php echo $child->name ?>
             <?php endforeach; ?>
-        <?php endif; 
+        <?php endif;
+        $this->initScriptsRadioBoxClass($compound_id, $property_id, $item_id, $index_id);
+    }
+
+    /**
+     * 
+     * @param type $property
+     * @param type $item_id
+     * @param type $index
+     */
+    public function initScriptsRadioBoxClass($compound_id,$property_id, $item_id, $index_id) {
+        ?>
+        <script>
+            $('input[name="radio-field-<?php echo $compound_id ?>-<?php echo $property_id ?>-<?php echo $index_id; ?>"]').change(function(){
+                $.ajax({
+                    url: $('#src').val() + '/controllers/object/form_item_controller.php',
+                    type: 'POST',
+                    data: {
+                        operation: 'saveValue',
+                        type:'term',
+                        value: $(this).val(),
+                        item_id:'<?php echo $item_id ?>',
+                        compound_id:'<?php echo $compound_id ?>',
+                        property_children_id: '<?php echo $property_id ?>',
+                        index: <?php echo $index_id ?>,
+                        indexCoumpound: 0
+                    }
+                });
+            });
+        </script>
+        <?php
     }
 }
