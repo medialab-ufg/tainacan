c75eaeb10e0b359f8afa2a090de6a294326b4f1a
Weryques <weryques@outlook.com>
2017-07-23 12:47:33 -0300

    (ref. issue #20) 1.3.2.1 Widget de Buscas Frequentes. Criar corpo do widget.

--- views/statistics/statistics_dashboard.php
+++ views/statistics/statistics_dashboard.php
@@ -7,15 +7,24 @@ include_once('js/dashboard_js.php');
     
     <div id="dash-buscas-frequentes" class="bgc-widget col-md-4 table-responsive">
       <table class="table table-hover">
-        <th>Buscas Frequentes</th>
-        <tr>
-          <td>Hello World!</td>
-          <td>100</td>
-        </tr>
-        <tr>
-          <td>Rélou Wordi! 20</td>
-          <td>200</td>
-        </tr>
+        <thead>
+          <tr>
+            <th class="text-left">Buscas Frequentes</th>
+            <th class="text-right">
+              <a class="glyphicon glyphicon-refresh"></a>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="text-left">Hello World!</td>
+            <td class="text-right">100</td>
+          </tr>
+          <tr>
+            <td class="text-left">Rélou Wordi! 20</td>
+            <td class="text-right">200</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   
