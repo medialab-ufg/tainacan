391e56b3dd63cf879af718abe8089eabcda25e8b
walisonjose <walison.josededeus@gmail.com>
2017-08-11 15:02:46 -0300

    Create deploy.php
    Testando autodeploy

---
+++ deploy.php
@@ -0,0 +1,44 @@
+<?php
+    /**
+     * GIT DEPLOYMENT SCRIPT
+     *
+     * Used for automatically deploying websites via GitHub
+     *
+     */
+
+    // array of commands
+    $commands = array(
+        'echo $PWD',
+        'whoami',
+        'git pull',
+        'git status',
+        'git submodule sync',
+        'git submodule update',
+        'git submodule status',
+    );
+
+    // exec commands
+    $output = '';
+    foreach($commands AS $command){
+        $tmp = shell_exec($command);
+
+        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    }
+?>
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<div style="width:700px">
+    <div style="float:left;width:350px;">
+    <p style="color:white;">Git Deployment Script</p>
+    <?php echo $output; ?>
+    </div>
+</div>
+</body>
+</html>
