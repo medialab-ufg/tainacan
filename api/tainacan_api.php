a7adfbf1dd26f557b3fcf2b8a14aa5954d3b3b7e
mbrunodm <mbrunodm@hotmail.com>
2017-08-16 10:11:24 -0300

    Remove Examples

--- api/tainacan_api.php
+++ api/tainacan_api.php
@@ -118,53 +118,53 @@ class TainacanApi extends WP_REST_Controller {
         ));
         //************* END REPOSITORY ENDPOINTS ****************//
         //************* START EXAMPLES ****************//
-        register_rest_route($namespace, '/' . $base, array(
-            array(
-                'methods' => WP_REST_Server::READABLE,
-                'callback' => array($this, 'get_items'),
-                'permission_callback' => array($this, 'get_items_permissions_check'),
-                'args' => array(
-                ),
-            ),
-            array(
-                'methods' => WP_REST_Server::CREATABLE,
-                'callback' => array($this, 'create_item'),
-                'permission_callback' => array($this, 'create_item_permissions_check'),
-                'args' => $this->get_endpoint_args_for_item_schema(true),
-            ),
-        ));
-        register_rest_route($namespace, '/' . $base . '/(?P<id>[\d]+)', array(
-            array(
-                'methods' => WP_REST_Server::READABLE,
-                'callback' => array($this, 'get_item'),
-                'permission_callback' => array($this, 'get_item_permissions_check'),
-                'args' => array(
-                    'context' => array(
-                        'default' => 'view',
-                    ),
-                ),
-            ),
-            array(
-                'methods' => WP_REST_Server::EDITABLE,
-                'callback' => array($this, 'update_item'),
-                'permission_callback' => array($this, 'update_item_permissions_check'),
-                'args' => $this->get_endpoint_args_for_item_schema(false),
-            ),
-            array(
-                'methods' => WP_REST_Server::DELETABLE,
-                'callback' => array($this, 'delete_item'),
-                'permission_callback' => array($this, 'delete_item_permissions_check'),
-                'args' => array(
-                    'force' => array(
-                        'default' => false,
-                    ),
-                ),
-            ),
-        ));
-        register_rest_route($namespace, '/' . $base . '/schema', array(
-            'methods' => WP_REST_Server::READABLE,
-            'callback' => array($this, 'get_public_item_schema'),
-        ));
+//        register_rest_route($namespace, '/' . $base, array(
+//            array(
+//                'methods' => WP_REST_Server::READABLE,
+//                'callback' => array($this, 'get_items'),
+//                'permission_callback' => array($this, 'get_items_permissions_check'),
+//                'args' => array(
+//                ),
+//            ),
+//            array(
+//                'methods' => WP_REST_Server::CREATABLE,
+//                'callback' => array($this, 'create_item'),
+//                'permission_callback' => array($this, 'create_item_permissions_check'),
+//                'args' => $this->get_endpoint_args_for_item_schema(true),
+//            ),
+//        ));
+//        register_rest_route($namespace, '/' . $base . '/(?P<id>[\d]+)', array(
+//            array(
+//                'methods' => WP_REST_Server::READABLE,
+//                'callback' => array($this, 'get_item'),
+//                'permission_callback' => array($this, 'get_item_permissions_check'),
+//                'args' => array(
+//                    'context' => array(
+//                        'default' => 'view',
+//                    ),
+//                ),
+//            ),
+//            array(
+//                'methods' => WP_REST_Server::EDITABLE,
+//                'callback' => array($this, 'update_item'),
+//                'permission_callback' => array($this, 'update_item_permissions_check'),
+//                'args' => $this->get_endpoint_args_for_item_schema(false),
+//            ),
+//            array(
+//                'methods' => WP_REST_Server::DELETABLE,
+//                'callback' => array($this, 'delete_item'),
+//                'permission_callback' => array($this, 'delete_item_permissions_check'),
+//                'args' => array(
+//                    'force' => array(
+//                        'default' => false,
+//                    ),
+//                ),
+//            ),
+//        ));
+//        register_rest_route($namespace, '/' . $base . '/schema', array(
+//            'methods' => WP_REST_Server::READABLE,
+//            'callback' => array($this, 'get_public_item_schema'),
+//        ));
         //************* END EXAMPLES ****************//
     }
 
