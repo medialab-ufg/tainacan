b5a1aa78b8caa76a0be839c9ce12f8a9b0136a86
Weryques <weryques@outlook.com>
2017-08-25 12:22:20 -0300

    Correção (Espaço não aparece após tradução).

--- footer.php
+++ footer.php
@@ -40,7 +40,7 @@
     <section class="logo-footer flogo-bar">
         <img class="logofooter"src="<?php echo get_template_directory_uri().'/libraries/images/Tainacan_pb.svg' ?>" width="60">
         <section class="wordpress-powered">
-            <?php bloginfo( 'name' ); _e(' is proudly powered by', 'tainacan'); ?>
+            <?php bloginfo( 'name' ); ?>&nbsp;<?php _e('is proudly powered by', 'tainacan'); ?>
             <a class="text-info" href="http://tainacan.org" target="_blank"> Tainacan </a> <?php _e('and', 'tainacan') ?>
             <a class="text-info" href="https://wordpress.org/" target="_blank"> WordPress </a>
         </section>
