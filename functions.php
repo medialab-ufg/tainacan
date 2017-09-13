1e0200e4c7c67efc397cc9ab8480c291a1cc296d
andre <andre2ar@outlook.com>
2017-09-13 15:18:06 -0300

    Facebook button test #185

--- functions.php
+++ functions.php
@@ -3760,7 +3760,7 @@ function facebook_meta() {
     global $post;
 
     if(is_single()) {
-        $img_info = (has_post_thumbnail($post->ID)) ? wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "full") : '';
+        $img_info = (has_post_thumbnail($post->ID)) ? wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), "thumbnail") : '';
         $image = array(
             'url' => (!empty($img_info[0])) ? $img_info[0] : '',
             'width' => (!empty($img_info[1])) ? $img_info[1] : 0,
@@ -3780,9 +3780,9 @@ function facebook_meta() {
         <meta property="og:site_name" content="<?php echo get_bloginfo(); ?>"/>
         <meta property="og:description" content="<?php echo $excerpt; ?>"/>
         <meta property="og:url" content="<?php echo the_permalink(); ?>"/>
-        <meta property="og:image" content="<?php echo $image['url']; ?>"/>
-        <!--<meta property="og:image:width" content="<?php /*echo $image['width']; */?>"/>
-        <meta property="og:image:height" content="<?php /*echo $image['height']; */?>"/>-->
+        <meta property="og:image" content="https://upload.wikimedia.org/wikipedia/en/2/2c/HotPursuit2010_screenshot.png"/>
+        <meta property="og:image:width" content="<?php echo $image['width']; ?>"/>
+        <meta property="og:image:height" content="<?php echo $image['height']; ?>"/>
         <?php
     } else {
         return;
