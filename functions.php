658983277117f117830a2cd9ac80728ed1b6b908
andre <andre2ar@outlook.com>
2017-09-13 16:11:41 -0300

    Facebook button test #185

--- functions.php
+++ functions.php
@@ -3784,6 +3784,7 @@ function facebook_meta() {
         <meta property="og:image" content="<?php echo get_the_post_thumbnail_url(get_the_ID()) ?>"/>
 
         <meta property="og:image:secure_url" content="<?php echo get_the_post_thumbnail_url(get_the_ID()) ?>" />
+
         <!--<meta property="og:image:width" content="200"/>
         <meta property="og:image:height" content="200"/>-->
         <?php
