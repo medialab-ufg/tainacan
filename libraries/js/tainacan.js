cf1c66e29dea9fd99420bde89368145786583355
andre <andre2ar@outlook.com>
2017-09-14 10:35:02 -0300

    Facebook share correction #185

--- libraries/js/tainacan.js
+++ libraries/js/tainacan.js
@@ -34,6 +34,14 @@ var Hook = {
 };
 
 $(window).load(function () {
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
     var src = $('#src').val();
 
     if (navigator.onLine) {
--- single-socialdb_object.php
+++ single-socialdb_object.php
@@ -332,24 +332,9 @@ if("one" === $single_mode) {
 
                             <div class="content-redesocial-NO" style="width: 100%">
                                 <div id="fb-root"></div>
-                                <script>(function(d, s, id) {
-                                        var js, fjs = d.getElementsByTagName(s)[0];
-                                        if (d.getElementById(id)) return;
-                                        js = d.createElement(s); js.id = id;
-                                        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1";
-                                        fjs.parentNode.insertBefore(js, fjs);
-                                    }(document, 'script', 'facebook-jssdk'));</script>
+                                <script></script>
 
-                                <div class="fb-share-button" data-href="<?php echo the_permalink(); ?>"
-                                     data-layout="button_count"
-                                     data-size="small"
-                                     data-mobile-iframe="true">
-                                    <a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse">
-                                        Compartilhar
-                                    </a>
-                                </div>
-
-                                <a class="facebook_share" target="_blank"
+                                <a class="fb" target="_blank"
                                    href="http://www.facebook.com/sharer.php?u=<?php echo the_permalink(); ?>">
                                     <img src="<?php echo get_template_directory_uri() . '/libraries/images/icons/icon-facebook.png'; ?>"/>
                                 </a>
