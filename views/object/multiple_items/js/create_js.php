0baa7091aa172ffc911ad3a70f15414ad4f7af06
andre <andre2ar@outlook.com>
2017-09-28 14:36:59 -0300

    PDF Enhanced #222

--- views/object/multiple_items/js/create_js.php
+++ views/object/multiple_items/js/create_js.php
@@ -7,11 +7,66 @@
         var myDropzone = new Dropzone("#dropzone_multiple_items", {
             maxFilesize: parseInt('<?php echo file_upload_max_size(); ?>') / 1024 / 1024,
             accept: function(file, done) {
-                //Cria miniatura para arquivos PDF
-                if(file.type == 'application/pdf')
-                {
-                    this.on("complete", function(file){
-                        //IndexedDB
+                if (file.type === ".exe") {
+                    done("Error! Files of this type are not accepted");
+                }
+                else { done(); }
+            },
+            init: function () {
+                thisDropzone = this;
+                this.on("removedfile", function (file) {
+                    //    if (!file.serverId) { return; } // The file hasn't been uploaded
+                    $.get($('#src').val() + '/controllers/object/object_controller.php?operation=delete_file&object_id=<?php echo $object_id ?>&file_name=' + file.id, function (data) {
+                        if (data.trim() === 'false') {
+                           // showAlertGeneral('<?php _e("Atention!", 'tainacan') ?>', '<?php _e("An error ocurred, File already removed or corrupted!", 'tainacan') ?>', 'error');
+                        } else {
+                           // showAlertGeneral('<?php _e("Success", 'tainacan') ?>', '<?php _e("File removed!", 'tainacan') ?>', 'success');
+                        }
+                    }); // Send the file id along
+                });
+                //ao terminar o uplaod dos itens
+                this.on("queuecomplete", function (file) {
+                    $('.extract-img-exif').show();
+                    $('#click_editor_items_button').show().focus();
+                        /*$.get($('#src').val()+'/controllers/object/object_controller.php?collection_id='+$('#collection_id').val()+'&operation=editor_items&object_id='+<?php echo $object_id ?>, function (data) {
+                            try {
+                                //var jsonObject = JSON.parse(data);
+                                if(data!=0){
+                                    $("#uploading").slideUp();
+                                    $('#editor_items').html(data);
+                                }else{
+                                    showAlertGeneral('<?php _e("Atention!", 'tainacan') ?>', '<?php _e("File is too big or Uploaded, however, not supported by wordpress, please select valid files!", 'tainacan') ?>', 'error');
+                                }
+                            }
+                            catch (e)
+                            {
+                                // handle error
+                            }
+                        });*/
+                });
+                $.get($('#src').val() + '/controllers/object/object_controller.php?operation=list_files&object_id=' + $("#object_id_add").val(), function (data) {
+                    try {
+                        //var jsonObject = JSON.parse(data);
+                        $.each(data, function (key, value) {
+                            if (value.name !== undefined && value.name !== 0) {
+                                var mockFile = {name: value.name, size: value.size,id:value.ID};
+                                thisDropzone.options.addedfile.call(thisDropzone, mockFile);
+                            }
+                        });
+                    }
+                    catch (e)
+                    {
+                        // handle error 
+                    }
+                });
+                this.on("success", function (file, message) {
+                          file.id = message.trim();
+                });
+
+                this.on("complete", function(file){
+                    //Cria miniatura para arquivos PDF
+                    if(file.type == 'application/pdf')
+                    {
                         window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
                         window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction;
                         window.IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange;
@@ -84,69 +139,7 @@
                                 });
                             };
                         }
-                    });
-                }
-
-                if (file.type === ".exe") {
-                    done("Error! Files of this type are not accepted");
-                }
-                else { done(); }
-            },
-            init: function () {
-                thisDropzone = this;
-                this.on("removedfile", function (file) {
-                    //    if (!file.serverId) { return; } // The file hasn't been uploaded
-                    $.get($('#src').val() + '/controllers/object/object_controller.php?operation=delete_file&object_id=<?php echo $object_id ?>&file_name=' + file.id, function (data) {
-                        if (data.trim() === 'false') {
-                           // showAlertGeneral('<?php _e("Atention!", 'tainacan') ?>', '<?php _e("An error ocurred, File already removed or corrupted!", 'tainacan') ?>', 'error');
-                        } else {
-                           // showAlertGeneral('<?php _e("Success", 'tainacan') ?>', '<?php _e("File removed!", 'tainacan') ?>', 'success');
-                        }
-                    }); // Send the file id along
-                });
-                //ao terminar o uplaod dos itens
-                this.on("queuecomplete", function (file) {
-                    $('.extract-img-exif').show();
-                    $('#click_editor_items_button').show().focus();
-                        /*$.get($('#src').val()+'/controllers/object/object_controller.php?collection_id='+$('#collection_id').val()+'&operation=editor_items&object_id='+<?php echo $object_id ?>, function (data) {
-                            try {
-                                //var jsonObject = JSON.parse(data);
-                                if(data!=0){
-                                    $("#uploading").slideUp();
-                                    $('#editor_items').html(data);
-                                }else{
-                                    showAlertGeneral('<?php _e("Atention!", 'tainacan') ?>', '<?php _e("File is too big or Uploaded, however, not supported by wordpress, please select valid files!", 'tainacan') ?>', 'error');
-                                }
-                            }
-                            catch (e)
-                            {
-                                // handle error
-                            }
-                        });*/
-                });
-                $.get($('#src').val() + '/controllers/object/object_controller.php?operation=list_files&object_id=' + $("#object_id_add").val(), function (data) {
-                    try {
-                        //var jsonObject = JSON.parse(data);
-                        $.each(data, function (key, value) {
-                            if (value.name !== undefined && value.name !== 0) {
-                                var mockFile = {name: value.name, size: value.size,id:value.ID};
-                                thisDropzone.options.addedfile.call(thisDropzone, mockFile);
-                            }
-                        });
                     }
-                    catch (e)
-                    {
-                        // handle error 
-                    }
-                });
-                this.on("success", function (file, message) {
-                          file.id = message.trim();
-                });
-
-                this.on("addedfile", function(file) {
-
-                    //window.open("/uploads/"+file.name);
-                    //console.log(file);
                 });
             },
             url: $('#src').val() + '/controllers/object/object_controller.php?operation=save_file&object_id=' +<?php echo $object_id ?>,
