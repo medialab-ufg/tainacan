2353b1016fd80b26616efe56d47c2445d59e6d81
andre <andre2ar@outlook.com>
2017-10-17 15:32:06 -0200

    PDF Enhenced #202

--- functions.php
+++ functions.php
@@ -3760,11 +3760,17 @@ function get_documents_text($ids)
         }
     }
 
+    //Recupera texto dos arquivos PDF
     foreach($PDFidPostAttachmentURL as $post_id => $info)
     {
         if(!array_key_exists('socialdb_pdf_text', $info['post_meta']))
         {
-            get_add_pdf_text($post_id, $info['attachment_id']);
+            try {
+	            get_add_pdf_text( $post_id, $info['attachment_id'] );
+            }catch (Exception $e)
+            {
+
+            }
         }
     }
 
