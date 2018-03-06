efd77d6f5a301e6d702e43525e39617171113f1e
andre <andre2ar@outlook.com>
2018-03-06 13:30:10 -0300

    Items count (TEST)

--- views/search/js/single_js.php
+++ views/search/js/single_js.php
@@ -120,6 +120,7 @@
             show_modal_main();
 
             let ids = $("#items_id").val().split(',');
+            console.log("Count items: "+ids.length);
 
             if($('input.bulk_action').val() === 'select_some')
             {
@@ -137,7 +138,7 @@
                     edit_data.push( { id: el } );
                 });
             }
-
+            console.log("Edita data length: "+edit_data.length);
             $.ajax({
                 type: "POST",
                 url: $('#src').val() + "/controllers/object/object_controller.php",
